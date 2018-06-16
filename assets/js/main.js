ae1bbd0bfefe6779cc71043f61e3b14c71eb5972
Barsh Visko <35918066+Barshavisko@users.noreply.github.com>
2018-06-16 22:51:51 +0930

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 /*
-	Introspect by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+	Created By Barshavisko,
+	Don't try anything cunt!
 */
 
 (function($) {
